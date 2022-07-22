0b2c7796e6bad18137422971450974630314ce6a
Ankit Kumar <kumarankit.ak325@gmail.com>
2022-07-22 21:01:20 +0530

    query states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+
+
+function Button() {
+  const { data, error }  = useQuery(['hello-world'] , ()=>{
+    return new Promise(resolve =>{setTimeout(()=>resolve(Math.random()), 2000)
+    })
+  })
+    console.log({error, data});
+    
+    return <button>I am a button {data}</button>
+}
+
+
 
 function App() {
-
-  const { data, error }  = useQuery(['hello-world'] ,()=>{
-    return Promise.resolve(5)
-    })
-    console.log({error, data});
-
-
+  const[visible,setVisible] = useState(true)
+  function hideButton(){
+    setVisible(visible => !visible)
+  }
   return (
     <div className="App">
       <header className="App-header">
+        {visible && <Button/>}
+        <button onClick={hideButton}>Toggle</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
